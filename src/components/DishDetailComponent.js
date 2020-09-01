4bc2b59cecd3d5d70554a1bd0f9bab61da4393c2
simon.montaggioni <simon.montaggioni@gmail.com>
2020-09-01 15:49:08 -0400

    feat: Display comments about the dish.
      - Create a renderComments method to display all the comments about
      the dish.
    
      - Use the appropiate bootstrap classes to make the content responsive
    
      Assignment 1 - Task 2 & 3.

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,23 +2,75 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle className='font-weight-bold text-left'>
+              {dish.name}
+            </CardTitle>
+            <CardText className='text-left'>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+  renderComments(comments) {
+    if (comments != null) {
+      let months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+      ];
+      return (
+        <div>
+          <h4 className='text-left'>Comments</h4>
+          <ul className='p-0'>
+            {comments.map((comment) => {
+              let commentDate = new Date(comment.date);
+              return (
+                <li key={comment.id} className='list-unstyled mb-3'>
+                  <div className='col-12 p-0 text-left'>{comment.comment}</div>
+                  <div className='col-12 p-0 text-left'>
+                    <span className='ml-1'>-- {comment.author},</span>
+                    <span className='ml-1'>
+                      {months[commentDate.getMonth()]}
+                    </span>
+                    <span className='ml-1'>{commentDate.getDay()},</span>
+                    <span className='ml-1'>{commentDate.getFullYear()}</span>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    } else return <div></div>;
+  }
   render() {
     return (
       <div className='row'>
         <div className='col-12 col-md-5 m-1'>
-          <Card>
-            <CardImg
-              top
-              src={this.props.dish.image}
-              alt={this.props.dish.name}
-            />
-            <CardBody>
-              <CardTitle>{this.props.dish.name}</CardTitle>
-              <CardText>{this.props.dish.description}</CardText>
-            </CardBody>
-          </Card>
+          {this.renderDish(this.props.dish)}
         </div>
-        <div className='col-md-6'></div>
+        <div className='col-12 col-md-5 m-1'>
+          {this.renderComments(
+            this.props.dish ? this.props.dish.comments : null
+          )}
+        </div>
       </div>
     );
   }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -15,11 +15,6 @@ class Menu extends Component {
     this.setState({ selectedDish: dish });
   }
 
-  renderDish(dish) {
-    if (dish != null) return <DishDetail dish={dish} />;
-    else return <div></div>;
-  }
-
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
@@ -37,11 +32,7 @@ class Menu extends Component {
     return (
       <div className='container'>
         <div className='row'>{menu}</div>
-        <div className='row'>
-          <div className='col-12 col-md-5 m-1'>
-            {this.renderDish(this.state.selectedDish)}
-          </div>
-        </div>
+        <DishDetail dish={this.state.selectedDish} />
       </div>
     );
   }
