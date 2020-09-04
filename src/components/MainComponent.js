a5faeb03e6d38ff2480db51478466b2b4cfc3e32
simon.montaggioni <simon.montaggioni@gmail.com>
2020-09-04 11:48:35 -0400

    feat: Create Contact component
      - Create Contact component and its route
      - Refactor the Main and Home components
      - Add the comment, leaders and promotions data to the shared folder
      - Update the dishes data

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
-import Header from './HeaderComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
     return (
       <div>
@@ -29,6 +42,7 @@ class Main extends Component {
             path='/menu'
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path='/contactus' component={Contact} />
           <Redirect to='/home' />
         </Switch>
         <Footer />
