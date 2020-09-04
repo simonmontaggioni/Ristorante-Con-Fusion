3f9e535fc5535aee2f73354880300e29fdc9e310
simon.montaggioni <simon.montaggioni@gmail.com>
2020-09-04 15:37:45 -0400

    fix: Show correctly the DishDetail component when a dish is clicked
      - Refactor the Menu component to use the router and pass the dishId
      and the comments  trought props
      - Refactor the Main component to include the DishDetail component in
      a route
      - Refactor the DishDetail component to use receive the dish data trought
      props
      - Add the Breadcrumb and the BreadcrumItem component to the Menu and
      the DishDetail components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import DishDetail from './DishDetailComponent';
 import Contact from './ContactComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -32,6 +33,20 @@ class Main extends Component {
         />
       );
     };
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
+      );
+    };
     return (
       <div>
         <Header />
@@ -42,6 +57,7 @@ class Main extends Component {
             path='/menu'
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />
           <Redirect to='/home' />
         </Switch>
